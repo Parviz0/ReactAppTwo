b6ccf0e1f272243ea4b5e5be5ea9247dc3731066
Parviz0 <100790765+Parviz0@users.noreply.github.com>
2022-12-05 14:51:30 +0300

    Add files via upload

---
+++ reactapp/src/components/_chiled_/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="profile">
+                <img src="/images/Frame 11.png" alt="" />
+                <span>Daler</span>
+                <img src="/images/Frame 12.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;
